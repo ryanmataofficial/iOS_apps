a4048693460b60469e36c190dd66b8edaa6acf3f
ryanmataofficial <97493948+ryanmataofficial@users.noreply.github.com>
2022-02-15 15:57:22 -0700

    Add completed "battlegame" app
    I created this app following the YouTube tutorial by CodeWithChris on 02/15/2022

---
+++ battlegame/Shared/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ryan Mata on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("dealbutton")
+                })
+
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.subheadline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ battlegame/Shared/battlegameApp.swift
@@ -0,0 +1,17 @@
+//
+//  battlegameApp.swift
+//  Shared
+//
+//  Created by Ryan Mata on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct battlegameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
